ea2cba5962b718094e54bddb3781a4a8f839d4bf
Professor Dave <dricha20@gmu.edu>
2017-11-15 23:20:51 -0500

    Product Template

---
+++ www/js/menu.js
@@ -0,0 +1,28 @@
+$(function() {
+
+	var tab_template = $('#tab_template').html();
+	var category_desc_template = $('#category_desc_template').html();
+	var product_template = $('#product_template').html();	
+
+	$.get('http://mis412.davidrichard.com/product_services/chico', function(data) {
+
+		console.log(data);
+
+		for (var i = 0; i < data.categories.length; i++) {
+
+			$('#category_tabs').append(Mustache.render(tab_template, data.categories[i]));
+			$('#product_content').append(Mustache.render(category_desc_template, data.categories[i]));
+
+			for (var j = 0; j < data.categories[i].products.length; j++) {
+			  $('#'+ data.categories[i].name).append(Mustache.render(product_template, data.categories[i].products[j]));
+			};
+
+		};
+
+		$('.tab-pane:first').addClass('active');
+		$('.tab-pane > div:odd > div').addClass('margin_top');
+		$('.nav-tabs li:first').addClass('active');
+
+	});
+
+});
